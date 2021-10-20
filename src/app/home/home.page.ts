97d0ec772b752de6a3011ef5a38a49fe3adca3f9
eneka <eneka.tamayo@gmail.com>
2021-10-20 19:13:06 +0200

    Primeros pasos en la maquetación

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+
+  constructor() { }
+
+  GetClass(p: number, pMax: number, pMin: number) {
+    let porcentage: Number;
+    porcentage=(p-pMin)*100/(pMax-pMin);
+    if(porcentage>75)
+      return 'rojo';
+    else if(porcentage>25)
+      return 'amarillo';
+    else
+      return 'verde';
+  }
+
+  FechaCorta(fecha: string): string {
+    var d=new Date(fecha);
+    return d.toLocaleDateString();
+  }
+
+  AnadirCero(i: number): string {
+    var x;
+    if(i<10) {
+      x="0"+i;
+    }
+    else {
+      x=i;
+    }
+    return x;
+  }
+
+  GetHora(fecha: string): string {
+    var d=new Date(fecha);
+    var x=this.AnadirCero(d.getHours())+':'+this.AnadirCero(d.getMinutes())
+    return x;
+  }
 
 }

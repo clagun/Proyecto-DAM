47ac656060c23736de4176a8dbf15674505b4603
eneka <eneka.tamayo@gmail.com>
2022-01-07 13:29:00 +0100

    cambios css y formato fecha

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -35,17 +35,17 @@ export class HomePage {
   electrodomestico: any;
   nombreelectro: any;
   hora: any;
-  textoDia : any;
+  textoDia: any;
   fecha: any;
 
   constructor(private apireeService: ApireeService,
     private apiaverage: ApiaverageService,
     private platform: Platform,
-    private routerOutlet: IonRouterOutlet, 
+    private routerOutlet: IonRouterOutlet,
     public toastController: ToastController) {
-   
-      this.cambiarFecha();
-      //se añade función de salir de la app, al dar botón atrás del hardware
+
+    this.cambiarFecha();
+    //se añade función de salir de la app, al dar botón atrás del hardware
     this.platform.backButton.subscribeWithPriority(-1, () => {
       if(!this.routerOutlet.canGoBack()) {
         navigator['app'].exitApp();
@@ -65,49 +65,49 @@ export class HomePage {
         this.horas.push(data.indicator.values[i].datetime);
       }
       this.precioMenor();
-      console.log("los precios de hoy: " + this.precios)
+      console.log("los precios de hoy: "+this.precios)
     });
   }
 
-  
+
   private precioMenor() {
-    var precioscomparar: Array<any>=[] ;
-    var horascomparar: Array<any>=[]; 
-    
-    if(this.textoDia == "Hoy"){
-      precioscomparar = this.precios;
-      horascomparar= this.horas;
-    }else{
-      precioscomparar = this.preciosmaniana;
-      horascomparar = this.horasmaniana;
+    var precioscomparar: Array<any>=[];
+    var horascomparar: Array<any>=[];
+
+    if(this.textoDia=="Hoy") {
+      precioscomparar=this.precios;
+      horascomparar=this.horas;
+    } else {
+      precioscomparar=this.preciosmaniana;
+      horascomparar=this.horasmaniana;
     }
-    this.precioMin = Math.min.apply(Math, precioscomparar);
-    var index = precioscomparar.indexOf(this.precioMin);
+    this.precioMin=Math.min.apply(Math, precioscomparar);
+    var index=precioscomparar.indexOf(this.precioMin);
 
     //Realiza un split de la fecha desde la T (2021-10-16T21:00:00.000+02:00) entonces almacena solo el tiempo.
-    var tiempo = horascomparar[index].split("T")[1];
+    var tiempo=horascomparar[index].split("T")[1];
     //Realiza un slice que quita el sobrante de la hora (segundos y cambio de hora) 21:00:00.000+02:00
-    this.horaMin = tiempo.slice(0, 5);
+    this.horaMin=tiempo.slice(0, 5);
 
-    this.precioMinEntre = precioscomparar[8];
-    this.horaMinEntre = horascomparar[0];
+    this.precioMinEntre=precioscomparar[8];
+    this.horaMinEntre=horascomparar[0];
 
 
-    for (let i = 8; i <= 22; i++) {
-      if (precioscomparar[i] <= this.precioMinEntre) {
-        this.precioMinEntre = precioscomparar[i];
-        this.horaMinEntre = horascomparar[i];
+    for(let i=8; i<=22; i++) {
+      if(precioscomparar[i]<=this.precioMinEntre) {
+        this.precioMinEntre=precioscomparar[i];
+        this.horaMinEntre=horascomparar[i];
       }
     }
 
-    this.horaMinEntre = this.horaMinEntre.split("T")[1];
-    this.horaMinEntre = this.horaMinEntre.slice(0, 5);
+    this.horaMinEntre=this.horaMinEntre.split("T")[1];
+    this.horaMinEntre=this.horaMinEntre.slice(0, 5);
 
     //si hay datos guardados en el local storage con la clave data, se ejecuta el método
-    if (localStorage.getItem('data')) {
+    if(localStorage.getItem('data')) {
       this.getPreciosHomeLocal();
     } else {
-      this.nombreelectro = "No elegido ";
+      this.nombreelectro="No elegido ";
     }
   }
 
@@ -141,7 +141,7 @@ export class HomePage {
       var suma=this.preciopromedioDiario.reduce((previous, current) => current+=previous);
       this.preciopromedio15dias=(suma/this.preciopromedioDiario.length)/1000;
 
-      if (localStorage.getItem('data')) {
+      if(localStorage.getItem('data')) {
         this.preciomax15dias=this.calculaPrecio(this.preciomax15dias, this.electrodomestico.consumo);
         this.preciomin15dias=this.calculaPrecio(this.preciomin15dias, this.electrodomestico.consumo);
         this.preciopromedio15dias=this.calculaPrecio(this.preciopromedio15dias, this.electrodomestico.consumo);
@@ -153,18 +153,18 @@ export class HomePage {
   ionViewWillEnter() {
     this.GetAPIPrecio();
     this.GetPrecioMedio();
-    this.hora = new Date().getHours();
-    if(this.hora >= 21){
+    this.hora=new Date().getHours();
+    if(this.hora>=21) {
       this.GetAPIPrecioManiana();
     }
-    
+
     // lanzamos la notificación
     cordova.plugins.notification.local.schedule({
       id: 1,
       smallIcon: "res://icon",
       icon: "res://icon",
       color: '4caf50',
-      title: "Ya puedes consultar los precios de la electricidad de mañana",
+      title: "Ya puedes consultar los precios de mañana",
       trigger: {every: {hour: 21, minute: 0}, count: 365},
       foreground: true
     });
@@ -186,19 +186,19 @@ export class HomePage {
     return (consumo/1000)*precio*1.10;
   }
 
-  cambiarFecha(){
-    
-    if (this.textoDia == "Hoy"){
-      this.textoDia = "Mañana";
-      this.apireeService.cambiarFecha = false;
+  cambiarFecha() {
+
+    if(this.textoDia=="Hoy") {
+      this.textoDia="Mañana";
+      this.apireeService.cambiarFecha=false;
       this.GetAPIPrecio();
-      this.fecha= this.apireeService.FechaCorta();
-      
-    }else {
-      this.textoDia = "Hoy";
-      this.apireeService.cambiarFecha = true;
+      this.fecha=this.apireeService.FechaCorta();
+
+    } else {
+      this.textoDia="Hoy";
+      this.apireeService.cambiarFecha=true;
       this.GetAPIPrecioManiana();
-      this.fecha= this.apireeService.FechaCorta();
-    }     
-  }  
+      this.fecha=this.apireeService.FechaCorta();
+    }
+  }
 }
